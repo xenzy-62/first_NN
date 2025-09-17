7307bec6abaf82347c72e0af06ca6a294c2f353e
xenzy-62 <meeemario8@gmail.com>
2025-09-17 09:12:11 +0100

    faster versio(a way faster version)

--- 1stneural.py
+++ 1stneural.py
@@ -1,15 +1,100 @@
 import numpy as np
 from PIL import Image
 from pathlib import Path
+from array import array
+import matplotlib.pyplot as plt
+from os.path  import join
+import struct
+
+
 
 i = 0
 exit = "c"
 clac_percent = 0
 savemaybe = "b"
-batch_size = 1000000
+batch_size = 60000
+training_batch = 100
+learning_handler = 0
+
+def visualize_1d_pixels(pixel_array, image_shape=(28, 28)):
+    """
+    Visualize a 1D array of pixel values
+    
+    Args:
+        pixel_array: 1D array of pixel values (0 to 1)
+        image_shape: expected shape of the image (height, width)
+    """
+    # Reshape to 2D
+    if len(pixel_array) == image_shape[0] * image_shape[1]:
+        image_2d = pixel_array.reshape(image_shape)
+    else:
+        # If not perfect square, find closest square
+        side_length = int(np.sqrt(len(pixel_array)))
+        image_2d = pixel_array[:side_length*side_length].reshape(side_length, side_length)
+    
+    # Plot the image
+    plt.figure(figsize=(6, 6))
+    plt.imshow(image_2d, cmap='gray', vmin=-1, vmax=1)
+    plt.colorbar(label='Pixel intensity')
+    plt.title('Pixel Visualization')
+    plt.axis('off')
+    plt.show()
+
+
+
+
+
+class MnistDataloader(object):
+    def __init__(self, training_images_filepath,training_labels_filepath,
+                 test_images_filepath, test_labels_filepath):
+        self.training_images_filepath = training_images_filepath
+        self.training_labels_filepath = training_labels_filepath
+        self.test_images_filepath = test_images_filepath
+        self.test_labels_filepath = test_labels_filepath
+    
+    def read_images_labels(self, images_filepath, labels_filepath):        
+        label = []
+        with open(labels_filepath, 'rb') as file:
+            magic, size = struct.unpack(">II", file.read(8))
+            if magic != 2049:
+                raise ValueError('Magic number mismatch, expected 2049, got {}'.format(magic))
+            label = array("B", file.read())        
+        
+        with open(images_filepath, 'rb') as file:
+            magic, size, rows, cols = struct.unpack(">IIII", file.read(16))
+            if magic != 2051:
+                raise ValueError('Magic number mismatch, expected 2051, got {}'.format(magic))
+            image_data = array("B", file.read())        
+        images = []
+        for i in range(size):
+            images.append([0] * rows * cols)
+        for i in range(size):
+            img = np.array(image_data[i * rows * cols:(i + 1) * rows * cols])
+            img = img.reshape(28, 28)
+            images[i][:] = img            
+        
+        return images, label
+            
+    def load_data(self):
+        x_train, y_train = self.read_images_labels(self.training_images_filepath, self.training_labels_filepath)
+        x_test, y_test = self.read_images_labels(self.test_images_filepath, self.test_labels_filepath)
+        return (x_train, y_train),(x_test, y_test)
+
+loader = MnistDataloader(
+    "D:\\data\\mnist\\train-images.idx3-ubyte",
+    "D:\\data\\mnist\\train-labels.idx1-ubyte", 
+    "D:\\data\\mnist\\t10k-images.idx3-ubyte",
+    "D:\\data\\mnist\\t10k-labels.idx1-ubyte"
+)
+
+
+
+
+
+
 
 def save_model():
-    name = input("enter model name: ")
+    name = "latest_3.2_turbo"
 
     script_path = Path(__file__).parent 
     new_folder = script_path / "models" / name
@@ -30,8 +115,9 @@ def load_model():
 
 
 def sigmoid(z):
-    z = np.clip(z, -50, 50)  # Prevents overflow
-    return 1 / (1 + np.exp(-z))
+    #z = np.clip(z, -50, 50)  # Prevents overflow
+    #1 / (1 + np.exp(-z))
+    return np.maximum(0,z) 
 
 def evaluate( input , layer1 , layer2 , weights1 , weights2 , weights3 , b1 , b2 , b3  ):
     z1=np.dot( weights1 , input  ) + b1
@@ -43,13 +129,31 @@ def evaluate( input , layer1 , layer2 , weights1 , weights2 , weights3 , b1 , b2
     z3=np.dot( weights3 , layer2 ) + b3
      
 
-    return layer1 , layer2 , sigmoid(z3)
+    return np.softmax(z3)
+
+def evaluate_t( input , layer1 , layer2 , weights1 , weights2 , weights3 , b1 , b2 , b3  ):
+
+    z1=np.dot( weights1 , input  ) + b1[: , np.newaxis]
+
+    layer1 = sigmoid(z1)
+
+    z2=np.dot( weights2 , layer1 ) + b2[: , np.newaxis]
+
+    layer2 = sigmoid(z2)
+
+    z3=np.dot( weights3 , layer2 ) + b3[: , np.newaxis]
+
+    return layer1 , layer2 , np.softmax(z3)
+
 
 
 def get_example_p(img_path):
     img = Image.open(img_path)
-    img_arr = 1-(np.array(img)[:,:,0].flatten() / 255.0)
-    return img_arr
+    img_arr = np.zeros((28,28))
+    for x in range(28):
+        for y in range(28):
+            img_arr [x , y ] = max(255-(np.array(img)[x , y , : ] ))
+    return img_arr.flatten()
 
 def get_example(img_path):
     img = Image.open(img_path)
@@ -69,53 +173,52 @@ def get_examples(dataset):
 
 def delta_b(pre_deltas , cur_layer , weights ):
 
-    prod=np.dot(pre_deltas , weights )
+    prod=np.dot( pre_deltas.T , weights )
 
-    return prod * cur_layer * ( 1 - cur_layer )
+    return prod.T * cur_layer * ( 1 - cur_layer )
         
 
-def delta(out_layer, data_label):
-    example = np.zeros(len(out_layer))
-    example[int(data_label)] = 1
-    return (out_layer - example)
+def delta(out_layer, ideal):
+    return (out_layer - ideal)
 
 def derive(deltas , pre_activations , act_num ) :
     L=len(deltas)
+    weight_clac = np.zeros((L , act_num , training_batch ))
     weights_dersss = np.zeros( (L , act_num) )
     for k in range(act_num):
         for j in range(L):
-            weights_dersss[ j , k ] = pre_activations[k]*deltas[j]
+            weight_clac[j , k , :] = pre_activations[k , :]*deltas[j , :]
+            weights_dersss[ j , k ] = np.sum(weight_clac [ j , k , : ])/training_batch
     return weights_dersss
 
 
 
 
 
-learning_rate = 0.1
+learning_rate = 0.01
 
 
 
-data = np.zeros(( 28*28 , batch_size ))
-data_label= np.zeros(batch_size)
+data = np.zeros((28*28 , batch_size))
 
 input_num= 28*28
 
-layer1_num= 8
-layer1=np.zeros(layer1_num)
+layer1_num= 128
+layer1=np.zeros((layer1_num , training_batch))
 
-layer2_num= 8
-layer2=np.zeros(layer2_num)
+layer2_num= 64
+layer2=np.zeros((layer2_num ,training_batch))
 
 num=10
-output=np.zeros(num)
+output=np.zeros((num ,training_batch))
 
-weights1=np.random.randn( layer1_num , input_num )
-weights2=np.random.randn(layer2_num , layer1_num )
-weights3=np.random.randn(num , layer2_num )
+weights1=np.random.randn(layer1_num , input_num)
+weights2=np.random.randn(layer2_num , layer1_num)
+weights3=np.random.randn(num , layer2_num)
 
-b1=np.random.randn(layer1_num)
-b2=np.random.randn(layer2_num)
-b3=np.random.randn(num)
+b1=np.random.randn(layer1_num)/100
+b2=np.random.randn(layer2_num)/100
+b3=np.random.randn(num)/100
 
 print("wanna load a model?")
 
@@ -125,72 +228,102 @@ if savemaybe == 'y' :
 
 
 
-predection= np.zeros(num)
-picture = np.zeros(28*28)
-batch = 1
+predection= np.zeros((num,training_batch))
+pictures = np.zeros((28*28 ,training_batch))
+batch = 0
+
+(x_train, y_train), (x_test, y_test) = loader.load_data()
+
+x_test = np.array(x_test)
+y_test = np.array(y_test)
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+for v in range(60000):
+    mean = np.mean(x_train[ v , : ])
+    std = np.std(x_train[ v , : ])
+    x_train[ v , : ] = -(mean - x_train[ v , : ] )/ std
+
 if savemaybe != "y":
-    datasett = Path(__file__).parent
-    data , data_label = get_examples( datasett / "dataset\\handwritten-digits-dataset-not-in-mnist\\versions\\4\\dataset")
-    while i == 0 :
+    
+    
+    
+    
+    
+
+
+    data = x_train[:batch_size].reshape(batch_size, 28*28)
+    labels = y_train[:batch_size]
+    
+    weights1_ders = np.zeros((layer1_num, input_num))
+    weights2_ders = np.zeros((layer2_num, layer1_num))
+    weights3_ders = np.zeros((num, layer2_num))
+    b1_ders = np.zeros(layer1_num)
+    b2_ders = np.zeros(layer2_num)
+    b3_ders = np.zeros(num)
+    
+    
+
+    out_deltas = np.zeros((num,training_batch))
+    layer2_deltas = np.zeros((layer2_num,training_batch))
+    layer1_deltas = np.zeros((layer1_num,training_batch))
+    error = np.zeros((num,training_batch))
+    training_batch_labels = np.zeros(training_batch)
+    ideal = np.zeros((num , training_batch))
+
+
+    while i != 10 :
 
         clac_percent = 0
-        error = 0
-        weights1_ders = np.zeros((layer1_num, input_num))
-        weights2_ders = np.zeros((layer2_num, layer1_num))
-        weights3_ders = np.zeros((num, layer2_num))
-        b1_ders = np.zeros(layer1_num)
-        b2_ders = np.zeros(layer2_num)
-        b3_ders = np.zeros(num)
-    
-    
+        ideal.fill(0)
 
-        out_deltas = np.zeros(num)
-        layer2_deltas = np.zeros(layer2_num)
-        layer1_deltas = np.zeros(layer1_num)
-        weights1_ders = np.zeros((layer1_num , input_num) )
-        weights2_ders = np.zeros((layer2_num , layer1_num) )
-        weights3_ders = np.zeros((num , layer2_num))
+        count = 0
+        for pic in range(batch*training_batch ,(batch+1)*training_batch):
+            pictures[: ,count] = data[pic, : ]
+            ideal [y_train[pic],count] = 1
+            count += 1
+            if pic == 59999:
+                batch = 0
+        layer1 , layer2 , predection=evaluate_t(pictures,layer1, layer2 , weights1 , weights2 , weights3 , b1 , b2 , b3)
+         
+        error = (ideal - predection)**2
         
-        error = np.zeros(num)
-
-        for ex_num in range((batch-1)*100,(batch*100)-1) :
-            if ex_num == batch_size - 10 :
-                data , data_label = get_examples("C:\\Users\\hassa\\.cache\\kagglehub\\datasets\\jcprogjava\\handwritten-digits-dataset-not-in-mnist\\versions\\4\\dataset")
-                batch = 1
-            picture = data[:, ex_num ]
-            layer1 , layer2 , predection=evaluate(picture,layer1, layer2 , weights1 , weights2 , weights3 , b1 , b2 , b3)
-            ideal = np.zeros(num)
-            example= int(data_label[ex_num])
-            ideal [example] = 1 
-            error += (ideal - predection)**2
-            if max(predection) != predection[example]:
-                clac_percent += 1
-            out_deltas = delta(predection,int(data_label[ex_num]))
-            layer2_deltas = delta_b(out_deltas , layer2 , weights3 ) 
-            layer1_deltas = delta_b(layer2_deltas , layer1,weights2)
-            weights1_ders += derive(layer1_deltas , picture , input_num ) 
-            weights2_ders += derive(layer2_deltas , layer1 , layer1_num )
-            weights3_ders += derive(out_deltas , layer2 , layer2_num )
-            b1_ders += layer1_deltas
-            b2_ders += layer2_deltas
-            b3_ders += out_deltas
+        out_deltas = delta(predection,ideal)
+        layer2_deltas = delta_b(out_deltas , layer2 , weights3 ) 
+        layer1_deltas = delta_b(layer2_deltas , layer1,weights2)
+        weights1_ders = derive(layer1_deltas , pictures , input_num ) 
+        weights2_ders = derive(layer2_deltas , layer1 , layer1_num )
+        weights3_ders = derive(out_deltas , layer2 , layer2_num )
+        b1_ders =np.sum(layer1_deltas, axis=1, keepdims=False)/training_batch
+        b2_ders = np.sum(layer2_deltas, axis=1, keepdims=False)/training_batch
+        b3_ders = np.sum(out_deltas, axis=1, keepdims=False)/training_batch
         batch += 1
-        percentage = 100 - clac_percent
-        print( "cost value: " + str(np.sum(error/100)))
-        print( "model accuracy percentage: " + str(percentage))
-        avg_error = np.sum(error)/100
-        if np.sum(avg_error) < 0.1 or percentage > 1 : 
+
+        percentage = 0
+        for test_sub in range(training_batch):
+            if np.where(predection[:,test_sub]==max(predection[:,test_sub]))[0][0] == np.where(ideal[:,test_sub ] == 1)[0][0]:
+                #print(ideal[:,test_sub])
+                #print(predection[:,test_sub])
+                
+                percentage += 1
+                #print(percentage)
+                #input()
+        avg_error = np.sum(error)/training_batch
+        print( "cost value: " + str(avg_error))
+        print("model accuracy percentage: "+str(percentage))
+        
+        if  percentage > 95 : 
             i +=1
         else :
-            weights1 = weights1 -(learning_rate * weights1_ders/100) 
-            weights2 = weights2 -(learning_rate * weights2_ders/100)
-            weights3 = weights3 -(learning_rate * weights3_ders/100)
-            b1 -= (learning_rate * b1_ders / 100)
-            b2 -= (learning_rate * b2_ders / 100)
-            b3 -= (learning_rate * b3_ders / 100)
-        
-        if avg_error < 0.3 :
-            learning_rate = learning_rate/10
+            weights1 = weights1 -(learning_rate * weights1_ders) 
+            weights2 = weights2 -(learning_rate * weights2_ders)
+            weights3 = weights3 -(learning_rate * weights3_ders)
+            b1 -= (learning_rate * b1_ders)
+            b2 -= (learning_rate * b2_ders)
+            b3 -= (learning_rate * b3_ders)
+
+    if avg_error < 0.1 and learning_handler==0:
+        learning_rate = learning_rate/100
+        learning_handler+=1     
 
 
 if savemaybe != 'y':
@@ -199,10 +332,13 @@ if savemaybe != 'y':
 while exit != '0' :    
     print("enter picture")
     picture_test = input("enter number path")
-
-
-    test=get_example_p(picture_test)
-    layer1 , layer2 , predection=evaluate(test,layer1, layer2 , weights1 , weights2 , weights3 , b1 , b2 , b3)
+    predection = np.zeros(num)
+    test_n = np.random.randint(1,1000)
+    #test=x_test[ test_n , : ].reshape(28*28)
+    test = get_example_p(picture_test)
+    visual = test
+    test = (test-np.mean(test))/np.std(test)
+    predection=evaluate(test,layer1, layer2 , weights1 , weights2 , weights3 , b1 , b2 , b3)
     print(predection)
     decision = 0
     v = 0
@@ -211,7 +347,8 @@ while exit != '0' :
             decision = v
         v +=1
     print(decision)
-
+    visualize_1d_pixels(test , (28,28) )
+    visualize_1d_pixels(visual , (28,28) )
     exit=input("type 0 to exit")
 
     
